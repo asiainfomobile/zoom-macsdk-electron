bcd67a15559398eee2a6cdc13591dddf275d3af1
zhengxin <zx19880528@gmail.com>
2018-05-16 14:06:37 +0800

    modified:   install-demo.sh

--- install-demo.sh
+++ install-demo.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env sh
 
-cp -r ZoomSDK $(npm config get prefix)/lib/node_modules/electron/node_modules
-cp -r $(npm config get prefix)/lib/node_modules/electron/node_modules demo
-cp -r ZoomSDK/* $(npm config get prefix)/lib/node_modules/electron/dist/Electron.app/Content/Frameworks
+path_in_zoom = "/usr/local/lib/node_modules/electron"
+prefix = $(npm config get prefix)
+if [ -d $path_in_zoom ]
+then
+  echo ""
+else
+  ln -sv $prefix/lib/node_modules/electron $path_in_zoom
+fi
+
+cp -r ZoomSDK $prefix/lib/node_modules/electron/node_modules
+cp -r $prefix/lib/node_modules/electron/node_modules demo
+cp -r ZoomSDK/* $prefix/lib/node_modules/electron/dist/Electron.app/Content/Frameworks
 
 
 
